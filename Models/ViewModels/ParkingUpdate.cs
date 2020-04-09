e58810ed410c6f6b28c105b9b94cd7bebb4d25b1
hetkansara <hetkansara.work@gmail.com>
2020-04-09 17:45:48 -0400

    Parking Registration completed

---
+++ Models/ViewModels/ParkingUpdate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalProject.Models.ViewModels
+{
+    public class ParkingUpdate
+    {
+        public virtual List<Parking> Parkings { get; set; }
+        public virtual ParkingBooking ParkingBooking { get; set; }
+    }
+}
